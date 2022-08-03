44581ac7f662463c4294035c8da1b2941b7a31ca
AntonijoLuksic <antonijo.luksic@gmail.com>
2022-08-04 00:01:58 +0200

    Rijesen Zadatak02

---
+++ Zadatak02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Ivo", "Ivic")
+            {
+                YearOfStudy=2,
+                Average=3.9
+            };
+            Console.WriteLine(student);
+            student = new Student();
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ Zadatak02/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak02
+{
+     class Student
+    {
+        public string Name { get; set; }
+        public string  Surname { get; set; }
+        public int YearOfStudy { get; set; }
+        public double Average { get; set; }
+
+        public Student():this("Pero", "Peric")
+        {
+
+        }
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public override string ToString()
+        {
+            return $"Student: {Name} {Surname}, Year of study: {YearOfStudy}, Average score: {Average}";
+        }
+    }
+}
